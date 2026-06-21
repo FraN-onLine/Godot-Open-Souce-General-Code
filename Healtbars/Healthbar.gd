b28aabb0896e4c72385220559e701c3eb536fd24
Franaffel <franksbass19@gmail.com>
2026-06-21 23:33:13 +0800

    healthbars

---
+++ Healtbars/Healthbar.gd
@@ -0,0 +1,17 @@
+extends TextureProgressBar
+
+@onready var timer = $Timer
+
+var health = 0 : set = _set_health
+
+func _set_health(new_health):
+	var previous_health = health
+	health = clamp(new_health, 0, max_value)
+	value = health
+
+
+# Called when the node enters the scene tree for the first time.
+func init_health(_health):
+	health = _health
+	max_value = health
+	value = health
